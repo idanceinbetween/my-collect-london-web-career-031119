c682186751096740c0d888bcd2aa64f354922407
github username <github email address>
2019-02-27 23:12:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)#1. I give you an array
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push yield(array[i]) #2. give me each element in the array
+    i +=1
+  end
+  new_array
+end
